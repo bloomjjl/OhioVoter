25c173c98aa411a7090265d065f779a47bba515e
Jerry Bloom <mrjerrybloom@yahoo.com>
2017-03-21 07:39:07 -0400

    Ballot & Email Updated Ballot email Created Email class in Services folder

--- OhioVoter/Controllers/BallotController.cs
+++ OhioVoter/Controllers/BallotController.cs
@@ -185,39 +185,12 @@ namespace OhioVoter.Controllers
 
         public bool SendEmailWithBallotSelections(BallotViewModel ballotVM)
         {
-            // make sure email address supplied
-            if (string.IsNullOrWhiteSpace(ballotVM.VoterEmailAddress)) { return false; }
+            string emailAddress = ballotVM.VoterEmailAddress;
+            string emailSubject = string.Format("Sample ballot for {0} Election", ballotVM.VotingDate);
+            string emailBody = string.Format("{0}", GetEmailBodyForBallot(ballotVM));
 
-            // get email message
-            System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
-            email.To.Add(ballotVM.VoterEmailAddress);
-            email.From = new System.Net.Mail.MailAddress("OhioVoter.org@yahoo.com");
-            email.Subject = string.Format("Sample ballot for {0} Election", ballotVM.VotingDate);
-            email.Body = string.Format("{0}", GetEmailBodyForBallot(ballotVM));
-            email.IsBodyHtml = false;
-
-            // setup email transfer
-            System.Net.Mail.SmtpClient smtpClient = new System.Net.Mail.SmtpClient()
-            {
-                //Host = "smtp.mail.yahoo.com", // Yahoo email
-                Host = "mail.twc.com", // Time Warner Cable
-                //Host = "smtp.fuse.net",  // Cincinnati Bell
-                //Credentials = new System.Net.NetworkCredential("OhioVoter.org@yahoo.com", "P@$$word!007"),
-                //Port = 25, // SSL = 465, TLS = 25, 587
-                //EnableSsl = true
-            };
-
-            try
-            {
-                // Send Message
-                smtpClient.Send(email);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception caught in CreateTestMessage1(): {0}", ex.ToString());
-                return false;
-            }
+            Services.Email email = new Services.Email();
+            return email.SendEmail(emailAddress, emailSubject, emailBody);
         }
 
 
---
+++ OhioVoter/Services/Email.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OhioVoter.Services
+{
+    public class Email
+    {
+
+        public Email()
+        {
+
+        }
+
+
+        public bool SendEmail(string address, string subject, string body)
+        {
+            // make sure email address supplied
+            if (string.IsNullOrWhiteSpace(address)) { return false; }
+
+            // get email message
+            System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
+            email.To.Add(address);
+            email.From = new System.Net.Mail.MailAddress("OhioVoter.org@yahoo.com");
+            email.Subject = subject;
+            email.Body = body;
+            email.IsBodyHtml = false;
+
+            // setup email transfer
+            System.Net.Mail.SmtpClient smtpClient = new System.Net.Mail.SmtpClient()
+            {
+                //Host = "smtp.mail.yahoo.com", // Yahoo email
+                Host = "mail.twc.com", // Time Warner Cable
+                //Host = "smtp.fuse.net",  // Cincinnati Bell
+                //Credentials = new System.Net.NetworkCredential("OhioVoter.org@yahoo.com", "P@$$word!007"),
+                //Port = 25, // SSL = 465, TLS = 25, 587
+                //EnableSsl = true
+            };
+
+            try
+            {
+                // Send Message
+                smtpClient.Send(email);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+    }
+}
